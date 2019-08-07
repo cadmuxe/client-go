647d390009462477b7d1d3185202144f033eece9
Koonwah Chen <koonwah@google.com>
2019-08-06 19:55:20 -0700

    dynamic-informer example

--- examples/out-of-cluster-client-configuration/main.go
+++ examples/out-of-cluster-client-configuration/main.go
@@ -18,23 +18,29 @@ limitations under the License.
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
-	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/tools/clientcmd"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/strategicpatch"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/dynamic/dynamicinformer"
+	"k8s.io/client-go/tools/cache"
+
 	//
 	// Uncomment to load all auth plugins
 	// _ "k8s.io/client-go/plugin/pkg/client/auth"
 	//
 	// Or uncomment to load specific auth plugins
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
 )
@@ -55,36 +61,51 @@ func main() {
 	}
 
 	// create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
+
+	dynamicClient, _ := dynamic.NewForConfig(config)
+
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "default"
-		pod := "example-xxxxx"
-		_, err = clientset.CoreV1().Pods(namespace).Get(pod, metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		}
+	destrinationGVR := schema.GroupVersionResource{Group: "networking.istio.io", Version: "v1alpha3", Resource: "destinationrules"}
+	drDynamicInformer := dynamicinformer.NewFilteredDynamicInformer(dynamicClient, destrinationGVR, "", 0,
+		cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc},
+		nil)
 
-		time.Sleep(10 * time.Second)
-	}
+	stopCh := make(chan struct{})
+
+	go drDynamicInformer.Informer().Run(stopCh)
+
+	drDynamicInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			fmt.Println("CRD DestinationRule ADD")
+			drus := obj.(*unstructured.Unstructured)
+			fmt.Printf("\t Name: %s\n", drus.GetName())
+			fmt.Printf("\t Host: %s\n", drus.Object["spec"])
+
+		},
+		UpdateFunc: func(old, cur interface{}) {
+			fmt.Println("CRD update.")
+			drus := cur.(*unstructured.Unstructured)
+			annotations := drus.GetAnnotations()
+			if _, ok := annotations["neg-status"]; !ok {
+				newDSus := drus.DeepCopy()
+				annotations["neg-status"] = "Hiiiiiiii"
+				newDSus.SetAnnotations(annotations)
+
+				patchBytes, _ := StrategicMergePatchBytes(drus, newDSus, unstructured.Unstructured{})
+				fmt.Printf("CRD NEG Annotation updated, diff: %s\n", string(patchBytes))
+				//dynamicClient.Resource(destrinationGVR).Patch(newDSus.GetName(), types.StrategicMergePatchType, patchBytes, metav1.PatchOptions{})
+				_, err := dynamicClient.Resource(destrinationGVR).Namespace(newDSus.GetNamespace()).Update(newDSus, metav1.UpdateOptions{})
+				if err != nil {
+					fmt.Printf("Update error: %s\n", err)
+				}
+			}
+		},
+	})
+
+	<-stopCh
 }
 
 func homeDir() string {
@@ -92,4 +113,23 @@ func homeDir() string {
 		return h
 	}
 	return os.Getenv("USERPROFILE") // windows
+}
+
+func StrategicMergePatchBytes(old, cur, refStruct interface{}) ([]byte, error) {
+	oldBytes, err := json.Marshal(old)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal old object: %v", err)
+	}
+
+	newBytes, err := json.Marshal(cur)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal new object: %v", err)
+	}
+
+	patchBytes, err := strategicpatch.CreateTwoWayMergePatch(oldBytes, newBytes, refStruct)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create patch: %v", err)
+	}
+
+	return patchBytes, nil
 }
